8a188ee91dad1b08f7c601abdff5427ea0520db9
iamiks <rmstjr1030@naver.com>
2025-02-28 17:20:41 +0900

    Fix : 외부 링크 연결 완료

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:examapp/widgets/episode_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:examapp/models/webtoon_detail.dart';
 import 'package:examapp/models/webtoon_episode.dart';
@@ -146,43 +147,7 @@ class _DetailScreenState extends State<DetailScreen> {
                     return Column(
                       children: [
                         for (var episode in snapshot.data!)
-                          Container(
-                            margin: const EdgeInsets.only(bottom: 10),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.green.shade400,
-                              boxShadow: [
-                                BoxShadow(
-                                  blurRadius: 5,
-                                  offset: const Offset(5, 5),
-                                  color: Colors.black.withOpacity(0.1),
-                                ),
-                              ],
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 10,
-                                horizontal: 20,
-                              ),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    episode.title,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  const Icon(
-                                    Icons.chevron_right_rounded,
-                                    color: Colors.white,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
+                          Episode(episode: episode, webtoonId: widget.id),
                       ],
                     );
                   }
